cba45143b920b2905c8f67628b567d4f74d2fa16
Mustafa Mwafy <66922777+mwafy092@users.noreply.github.com>
2022-05-23 15:03:06 +0200

    adding base skeleton and navbar

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <NavBar />
+    <header>header</header>
+    <section>See latest vacation ideas</section>
+    <section>Recent holiday locations</section>
+    <section>Why we are different</section>
+    <section>Contact us</section>
+    <footer>footer</footer>
+  </div>
+</template>
+
+<script>
+import NavBar from "./components/NavBar.vue";
+
+export default {
+  name: "App",
+  components: {
+    NavBar,
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  max-width: 1920px;
+  padding: 0;
+}
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,44 @@
+<template>
+  <nav class="navbar navbar-expand-sm navbar-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Pirate Holiday</a>
+
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#collapsibleNavbar"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse navbar-items" id="collapsibleNavbar">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="#">Experiences</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="#">Questions</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+};
+</script>
+
+<style lang="scss">
+nav {
+  background: #fff;
+}
+</style>
